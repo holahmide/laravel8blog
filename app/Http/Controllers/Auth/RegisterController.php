42d0e574d0b06bb027f70af20c49e5fc955dc591
Adeniyi Olamide <olami02bj@gmail.com>
2020-11-25 10:47:51 +0100

    second commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,10 @@ use App\Models\User;
 
 class RegisterController extends Controller
 {
+    public function __construct() {
+        $this->middleware(['guest']); 
+    }
+
     public function index() {
         return view('auth.register');
     }
@@ -27,6 +31,8 @@ class RegisterController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password)
         ]);
+
+        auth()->attempt($request->only('email','password'));
 
         return redirect()->route('dashboard');
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,32 +9,40 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-gray-200">
-    <nav class="p-6 bg-white flex justify-between mb-6">
+    <nav class="p-2 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li class="p-3">
-                <a href="">Home</a>
+                <a href="/">Home</a>
             </li>
             <li class="p-3">
-                <a href="">Dashboard</a>
+                <a href="{{ route('dashboard') }}">Dashboard</a>
             </li>
             <li class="p-3">
-                <a href="">Post</a>
+                <a href="{{ route('posts') }}">Post</a>
             </li>
         </ul>
 
         <ul class="flex items-center">
-            <li class="p-3">
-                <a href="">Adeniyi Olamide</a>
-            </li>
-            <li class="p-3">
-                <a href="">Login</a>
-            </li>
-            <li class="p-3">
-                <a href="{{ route('register') }}">Register</a>
-            </li>
-            <li class="p-3">
-                <a href="">Logout</a>
-            </li>
+            @auth
+                <li class="p-3">
+                    <a href="">{{ auth()->user()->username }}</a>
+                </li>
+                <li class="p-3">
+                    <form action="{{ route('logout') }}" method="post" class="inline">
+                        @csrf
+                        <button class="p-3">Logout</button>
+                    </form>
+                </li>
+            @endauth
+
+            @guest
+                <li class="p-3">
+                    <a href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="p-3">
+                    <a href="{{ route('register') }}">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
     @yield('content') 
